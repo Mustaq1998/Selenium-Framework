2281efa3780e1669de766cb198ec2483f3bbf29f
Mustaq1998 <mustaqalibaig3698@gmail.com>
2025-02-16 12:41:36 -0600

    Initial commit

---
+++ src/main/java/Automation/Selenium_Framework/App.java
@@ -0,0 +1,10 @@
+package Automation.Selenium_Framework;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/Automation/Selenium_Framework/Reverse_String.java
@@ -0,0 +1,20 @@
+package Automation.Selenium_Framework;
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+		String name = "Zakriya";
+		String reverse = "";
+		
+		int length=name.length();
+		
+		for (int i=length-1; i>0; i--)
+		{
+			reverse=reverse+name.charAt(i);
+		}
+		
+		System.out.println("Reverse string is: "+ reverse);
+
+	}
+
+}
