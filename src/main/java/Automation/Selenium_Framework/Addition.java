e13e786820a58ade1e68ee002ef9dab0168bc699
Mustaq1998 <mustaqalibaig3698@gmail.com>
2025-02-16 13:16:48 -0600

    Subtraction
    Updated

--- src/main/java/Automation/Selenium_Framework/Addition.java
+++ src/main/java/Automation/Selenium_Framework/Addition.java
@@ -4,16 +4,40 @@ public class Addition {
 
 	public static void main(String[] args) {
 		
+  // addition();
+   subtraction();
+   
+   
+   
+   
+	
+	
+	
+
+		
+	}
+
+	private static void subtraction() {
+	
+		
+		int a = 20;
+		int b = 10;
+		
+	int sum = a-b;
+	
+	System.out.println(sum);
+		
+	}
+
+	private static void addition() {
+	
+		
 		int a = 10;
 		int b = 20;
 		
 	int sum = a+b;
 	
 	System.out.println(sum);
-	
-		
-		
-		
 		
 	}
 }
